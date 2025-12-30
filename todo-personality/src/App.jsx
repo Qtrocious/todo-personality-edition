57337c5b8847579f2724a7ee2193f05bc202d7a3
Alonso Banda <113001530+Qtrocious@users.noreply.github.com>
2025-12-29 22:59:47 -0600

    Learning and testing things with input checkboxes and the initial layout of the app, it's going to be called ToDo: Personality Edition!

---
+++ todo-personality/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+    <h1>annoying todo list</h1>
+    <div className='list'>
+      <input type="checkbox"/> 
+      <p className='task'>ToDo List</p>
+    </div>
+    </>
+  )
+}
+
+export default App
